f34f9296f8d2e35e1c388e9427f8a19e17b8e7fd
Markus Wolf <markus.wolf@sinnerschrader.com>
2016-09-05 18:30:11 +0200

    feat: add demo rendering

--- extension.ts
+++ extension.ts
@@ -1,146 +1,181 @@
 'use strict';
-import { exists } from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
+import * as http from 'http';
+import * as url from 'url';
+import * as execa from 'execa';
+
+let patternplate: any;
+const port = '1337';
 
 export function activate(context: vscode.ExtensionContext) {
-    const provider = new PatternplateDemoContentProvider(context);
-    let disposable = vscode.workspace.registerTextDocumentContentProvider('patternplate', provider);
-    context.subscriptions.push(disposable);
+	const provider = new PatternplateDemoContentProvider(context);
+	let disposable = vscode.workspace.registerTextDocumentContentProvider('patternplate-demo', provider);
+	context.subscriptions.push(disposable);
 
-    disposable = vscode.commands.registerCommand('patternplate.showDemo', showDemo);
-    context.subscriptions.push(disposable);
+	disposable = vscode.commands.registerCommand('patternplate.showDemo', showDemo);
+	context.subscriptions.push(disposable);
 
-    disposable = vscode.commands.registerCommand('patternplate.showDemoToSide',
-        (uri: vscode.Uri) => showDemo(uri, true));
-    context.subscriptions.push(disposable);
+	disposable = vscode.commands.registerCommand('patternplate.showDemoToSide',
+		(uri: vscode.Uri) => showDemo(uri, true));
+	context.subscriptions.push(disposable);
 
-    vscode.workspace.onDidSaveTextDocument(document => {
-        updateDemo(document, provider);
-    });
-    vscode.workspace.onDidChangeTextDocument(event => {
-        updateDemo(event.document, provider);
-    });
-    vscode.workspace.onDidChangeConfiguration(() => {
-        vscode.workspace.textDocuments.forEach(document => {
-            if (document.uri.scheme === 'patternplate-demo') {
-                provider.update(document.uri);
-            }
-        });
-    });
+	vscode.workspace.onDidSaveTextDocument(document => {
+		updateDemo(document, provider);
+	});
+	vscode.workspace.onDidChangeTextDocument(event => {
+		updateDemo(event.document, provider);
+	});
+	vscode.workspace.onDidChangeConfiguration(() => {
+		vscode.workspace.textDocuments.forEach(document => {
+			if (document.uri.scheme === 'patternplate-demo') {
+				provider.update(document.uri);
+			}
+		});
+	});
+
+	console.log(`Starting patternplate in ${vscode.workspace.rootPath}`);
+	patternplate = execa('node', ['--harmony', './node_modules/.bin/patternplate', 'start', '--server.port', port], {
+		cwd: vscode.workspace.rootPath
+	});
+	patternplate.catch(error => {
+		console.error(error);
+		vscode.window.showErrorMessage(error);
+	});
 }
 
 export function deactivate() {
+	if (patternplate) {
+		console.log('Stopping patternplate');
+		patternplate.kill();
+		patternplate = undefined;
+	}
 }
 
 function updateDemo(document: vscode.TextDocument, provider: PatternplateDemoContentProvider): void {
-    if (isPatternplateDemo(document)) {
-        provider.update(getPatternplateDemoUri(document.uri));
-    }
+	if (isPatternplateDemo(document)) {
+		provider.update(getPatternplateDemoUri(document.uri));
+	}
 }
 
 function isPatternplateDemo(document: vscode.TextDocument) {
-    return document.languageId === 'patternplate'
-        && document.uri.scheme !== 'patternplate-demo';
+	return document.uri.scheme !== 'patternplate-demo';
 }
 
 function getPatternplateDemoUri(uri: vscode.Uri): vscode.Uri {
-    return Object.assign({}, uri, {
-        scheme: 'patternplate-demo',
-        path: uri.path,
-        query: uri.toString()
-    });
+	const patternId = uri.fsPath.match(/.*\/patterns\/([^\/]+\/[^\/]+)\/.*/);
+	return (uri as any).with({
+		scheme: 'patternplate-demo',
+		path: path.dirname(uri.path),
+		fsPath: path.dirname(uri.fsPath),
+		query: patternId[1]
+	});
 }
 
 function showDemo(uri: vscode.Uri, sideBySide: boolean = false): Thenable<any> {
-    var resource = uri;
-    if (!(resource instanceof vscode.Uri)) {
-        if (vscode.window.activeTextEditor) {
-            resource = vscode.window.activeTextEditor.document.uri;
-        }
-    }
-    if (!(resource instanceof vscode.Uri)) {
-        return;
-    }
-	const testUri = vscode.Uri.parse(`patternplate://demo/home/marwol/source/vw/vwa-patternplate/patterns/atoms/button/index.jsx`);
-	console.log('SHOW PREVIEW', resource, getPatternplateDemoUri(resource), testUri);
-    return vscode.commands.executeCommand('vscode.previewHtml', /*getPatternplateDemoUri(resource)*/ testUri,
-        	getViewColumn(sideBySide), "Preview '" + path.basename(resource.fsPath) + "'")
-		.then(success => {}, error => {
+	var resource = uri;
+	if (!(resource instanceof vscode.Uri)) {
+		if (vscode.window.activeTextEditor) {
+			resource = vscode.window.activeTextEditor.document.uri;
+		}
+	}
+	if (!(resource instanceof vscode.Uri)) {
+		return;
+	}
+	const demoUri = getPatternplateDemoUri(resource);
+	return vscode.commands.executeCommand('vscode.previewHtml', demoUri,
+			getViewColumn(sideBySide), `Demo '${demoUri.query}'`)
+		.then(success => { }, error => {
 			console.warn(error);
 			vscode.window.showErrorMessage(error);
 		});
 }
 
 function getViewColumn(sideBySide: boolean): vscode.ViewColumn {
-    var active = vscode.window.activeTextEditor;
-    if (!active) {
-        return vscode.ViewColumn.One;
-    }
-    if (!sideBySide) {
-        return active.viewColumn;
-    }
-    switch (active.viewColumn) {
-        case vscode.ViewColumn.One:
-            return vscode.ViewColumn.Two;
-        case vscode.ViewColumn.Two:
-            return vscode.ViewColumn.Three;
-    }
-    return active.viewColumn;
+	var active = vscode.window.activeTextEditor;
+	if (!active) {
+		return vscode.ViewColumn.One;
+	}
+	if (!sideBySide) {
+		return active.viewColumn;
+	}
+	switch (active.viewColumn) {
+		case vscode.ViewColumn.One:
+			return vscode.ViewColumn.Two;
+		case vscode.ViewColumn.Two:
+			return vscode.ViewColumn.Three;
+	}
+	return active.viewColumn;
 }
 
 class PatternplateDemoContentProvider implements vscode.TextDocumentContentProvider {
 
-    public _onDidChange: vscode.EventEmitter<vscode.Uri> = new vscode.EventEmitter<vscode.Uri>();
+	public _onDidChange: vscode.EventEmitter<vscode.Uri> = new vscode.EventEmitter<vscode.Uri>();
 
-    private context: vscode.ExtensionContext;
+	private waiting: boolean = false;
 
-    private waiting: boolean = false;
+	private renderer: any;
 
-    private renderer: any;
+	constructor(private context: vscode.ExtensionContext) {
+		this.context = context;
+		this.renderer = this.createRenderer();
+	}
 
-    constructor(context: vscode.ExtensionContext) {
-        this.context = context;
-        this.renderer = this.createRenderer();
-    }
+	private createRenderer() {
+		let firstRender = true;
+		return {
+			render(patternId: string) {
+				return new Promise(resolve => {
+					if (firstRender) {
+						firstRender = false;
+						setTimeout(() => {
+							resolve();
+						}, 8000);
+					} else {
+						resolve();
+					}
+				})
+				.then(() => {
+					return new Promise(resolve => {
+						const base = `http://localhost:${port}`;
+						const options: any = url.parse(`${base}/demo/${patternId}`);
+						if (!options.headers) {
+							options.headers = {};
+						}
+						options.headers['Accept'] = 'text/html';
 
-    private createRenderer() {
-        return {
-            render(text: string) {
-                return text;
-            }
-        };
-    }
+						http.get(options, res => {
+							let body = '';
+							res.on('data', (data: any) => {
+								body += data.toString();
+							});
+							res.on('end', () => {
+								resolve(body.replace(/<head>/, `<head><base href="${base}/">`));
+							});
+							res.resume();
+						});
+					});
+				});
+			}
+		};
+	}
 
-    public update(uri: vscode.Uri): void {
-        if (!this.waiting) {
-            this.waiting = true;
-            setTimeout(() => {
-                this.waiting = false;
-                this._onDidChange.fire(uri);
-            }, 300);
-        }
-    }
+	public update(uri: vscode.Uri): void {
+		if (!this.waiting) {
+			this.waiting = true;
+			setTimeout(() => {
+				this.waiting = false;
+				this._onDidChange.fire(uri);
+			}, 300);
+		}
+	}
 
-    get onDidChange(): vscode.Event<vscode.Uri> {
-        return this._onDidChange.event;
-    }
+	get onDidChange(): vscode.Event<vscode.Uri> {
+		return this._onDidChange.event;
+	}
 
-    public provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): string | Thenable<string> {
-        return vscode.workspace.openTextDocument(vscode.Uri.parse(uri.query)).then((document) => {
-            return `
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-                        <base href="${document.uri.toString()}">
-                    </head>
-                    <body>
-                        ${this.renderer.render(document.getText())}
-                    </body>
-                </html>
-            `;
-        });
-    }
+	public provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): string | Thenable<string> {
+		return this.renderer.render(uri.query);
+	}
 
 }
